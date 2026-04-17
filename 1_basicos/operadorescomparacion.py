40c9056dbf5d13b96ed784c45dbcfa69ac75ec96
David Nieto <nietomoreno84.gmail.com>
2026-04-17 10:40:23 +0200

    creacion de ficheros de python con operadores matematicos y de comparacion

---
+++ 1_basicos/operadorescomparacion.py
@@ -0,0 +1,18 @@
+numA = 18
+numB = 5
+
+# mayor que
+resultado = numA>numB
+print("mayor que:",resultado)
+
+#menor que
+resultado = numA < numB
+print ("menor que:" , resultado)
+
+#igualdad
+resultado = numA==numB
+print ("igual que:" , resultado)
+
+# distinto
+resultado = numA != numB
+print("distinto:",  resultado)
