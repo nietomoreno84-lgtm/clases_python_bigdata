4daefd76cdf7504fbf0aa40ce79729a5933cb9fd
David Nieto <nietomoreno84.gmail.com>
2026-04-22 11:34:22 +0200

    ejercicios 4 y 5

---
+++ 2_flujo/bucles/ej4_bucles.py
@@ -0,0 +1,20 @@
+# necesito que pidais varios numeros por pantalla y que los sumeis. El programa termina cuando metemos el numero 0;
+# while.
+# paso 1: pedir numero constantemente
+# paso 2: crear una variables suma = 0
+# paso 3: añadir a suma el valor introducido previmente convertido.
+# paso 4: pulsando 0 acabamos el ejercicio.
+print ("-"*30)
+print("EJERCICIO 4 - SUMA ACUMULUDA")
+print("-"*30)
+
+suma = 0
+while True:
+    numero = int(input("Introduce un número: "))
+    
+    if numero == 0:
+        break
+    
+    suma += numero
+
+print(suma)
