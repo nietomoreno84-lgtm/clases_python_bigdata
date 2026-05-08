bb431a4a632a2ea2a8d27aedb699953d5f2bc86f
david nieto moreno <nietomoreno84@gmail.com>
2026-05-08 13:20:42 +0200

    ejercicio dni

--- ejercicios/ej5/ej5.py
+++ ejercicios/ej5/ej5.py
@@ -22,18 +22,48 @@ se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es 
 """
 
 
-def main():
-    dni_completo = input('introduce tu dni: ')
-    letra= ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B','N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E','T']
-    if len(dni_completo) >9 :
-        numeros = dni_completo[:-1]
-        letra = dni_completo [:-1]
-        print(f'los numeros son (numeros) y la letra es (letra).')
-        calculo = int(numeros) % 23
-        print(f'el indice de tu letras es (calculo).')
-        for i in range (0,len(letra)):
-            if calculo == i:
-                print(letra[i])
-        print(letra)
-main()
+"""
+El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo que se basa en obtener el resto de la división entera del número de DNI y el número 23. 
+A partir del resto de la división, se obtiene la letra seleccionándola dentro de un lista de letras.
 
+El array de letras es:
+letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B','N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E','T'];
+
+Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A. 
+Con estos datos, elaborar un pequeño script que:
+
+1. Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del
+DNI que se ha indicado.
+
+2. Si el numero no introducido no esta formado por numeros solo deberá para la ejecución y lanzar 
+un error de numero no valido.
+
+3. Si el número es válido, se calcula la letra que le corresponde según el método explicado
+anteriormente.
+
+4. Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, 
+se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta. En otro caso, se muestra un mensaje indicando que el número y la letra de DNI son correctos.     
+"""
+
+def main():
+    dni = input('dime tu dni: ').upper()
+    dni = dni.zfill(9)
+    print(dni)
+    letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B','N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'] 
+    # paso 1: comprobar si el dni tiene 9 o menos caracteres
+    if len(dni) > 9:
+        print('Dni no valido')
+        return 
+    # paso 2: extraer la letra y el numero del dni
+    letra = dni[-1]
+    numero = int(dni[:-1]) # numero sin la letra
+    i = numero % 23
+    letra_correcta = letras[i]
+    
+    if letra != letra_correcta:
+        print(f'Dni {dni} no valido')
+    else:
+        print(f'Dni {dni} valido')
+    
+
+main()
