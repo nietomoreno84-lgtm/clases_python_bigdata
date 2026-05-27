b4cf33e1c5650bef74b32ff30b54e1b99aad917c
david nieto moreno <nietomoreno84@gmail.com>
2026-05-27 14:06:57 +0200

    FINALIZACION PROYECTO

--- practica_final/opcion_B_esports/main.py
+++ practica_final/opcion_B_esports/main.py
@@ -8,27 +8,33 @@ from lib.auditoria import hacer_auditoria
 from lib.exportacion import generar_informe_txt
 
 
-
+#VARIABLES DE ARCHIVOS SUCIOS
 
 premios_sucio= cargar_csv('datos', 'premios.csv')
 partidas_sucios = cargar_json('datos', 'partidas.json')
 jugadores_sucios = cargar_csv('datos','jugadores.csv')
 
-
-
-
-
-
 #  PROCESAR 1 HOJA (Hoja activa)
 print("\n--- Cargando 1 Hoja: EQUIPOS ---")
 equipos_sucios = cargar_excel('datos', 'equipos.xlsx')
+
+
+
+# VARIABLES DE ARCHIVOS LIMPIOS
 
 equipos_limpios = procesar_equipo(equipos_sucios)
 premios_limpio = procesar_premios(premios_sucio)
 jugadores_limpio = procesar_jugadores(jugadores_sucios)
 partidas_limpio = procesar_partidas(partidas_sucios)
 
-        
+
+numero_correciones_equipos = cuantificacion_correciones(equipos_sucios, equipos_limpios)
+numero_correciones_jugadores = cuantificacion_correciones(jugadores_sucios,jugadores_limpio)
+numero_correciones_partidas = cuantificacion_correciones(partidas_sucios,partidas_limpio)
+numero_correciones_premios = cuantificacion_correciones(premios_sucio,premios_limpio)
+
+#LLAMADAS DE FUNCIONES
+       
 crear_csv(jugadores_limpio, 'jugadores.csv', 'datos_limpios')
 crear_csv(equipos_limpios, 'equipos.csv', 'datos_limpios') 
 crear_csv(partidas_limpio, 'partidas.csv', 'datos_limpios')
@@ -39,14 +45,11 @@ crear_excel('datos_limpios', 'Datos_completo.xlsx',jugadores_limpio,'jugadores' 
 crear_excel('datos_limpios', 'Datos_completo.xlsx',partidas_limpio,'partidas' )
 crear_excel('datos_limpios', 'Datos_completo.xlsx',premios_limpio,'premios' )
 
-numero_correciones_equipos = cuantificacion_correciones(equipos_sucios, equipos_limpios)
-numero_correciones_jugadores = cuantificacion_correciones(jugadores_sucios,jugadores_limpio)
-numero_correciones_partidas = cuantificacion_correciones(partidas_sucios,partidas_limpio)
-numero_correciones_premios = cuantificacion_correciones(premios_sucio,premios_limpio)
 
 
+# VARIABLES PARA AUDITORIA
 
-auditoria_global = {
+auditoria = {
     "equipos.xlsx": hacer_auditoria(equipos_sucios, "equipos.xlsx"),
     "jugadores.csv": hacer_auditoria(jugadores_sucios, "jugadores.csv"),
     "partidas.json": hacer_auditoria(partidas_sucios, "partidas.json"),
@@ -64,4 +67,4 @@ cambios_totales = {
 
 
 # print(equipos_limpios)
-generar_informe_txt(auditoria_global, cambios_totales, "datos_limpios/informe_limpieza.txt")
+generar_informe_txt(auditoria, cambios_totales, "datos_limpios/informe_limpieza.txt")
